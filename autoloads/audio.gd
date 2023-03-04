de3eed0792c9bb0c825bce520c63376213f18967
Davide Cristini <davcri91@gmail.com>
2023-03-04 09:58:28 +0100

    chore: remove all files, keep assets

--- autoloads/audio.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-
-func play(node_name: String):
-	print("playing", node_name)
-	var audio_node = get_node(node_name) as AudioStreamPlayer
-	if !audio_node:
-		print_debug("Node", node_name, "not found")
-	audio_node.play()
--- objects/flag/flag.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-class_name Flag
-
-signal level_complete
-
-func _on_area_3d_body_entered(body):
-	if body is Player:
-		emit_signal("level_complete")
